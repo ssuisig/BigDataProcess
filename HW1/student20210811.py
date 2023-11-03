5f2096caf996e6c35af3477aef55f64eee7aca15
ssuisig <jsy6430000@naver.com>
2023-11-03 14:16:11 +0900

    HW1_up4

--- HW1/student20210811.py
+++ HW1/student20210811.py
@@ -5,6 +5,15 @@ wb = load_workbook(filename='student.xlsx')
 list1=[]
 list2=[]
 listGrade=[]
+listA1=[]
+listB1=[]
+listC1=[]
+A1=0
+B1=0
+C1=0
+A2=0
+B2=0
+C2=0
 listGrade.append(('0',0))
 
 ws = wb.active
@@ -35,21 +44,44 @@ for i in range(1, len(list1)):
             if i <= lenA //2:
                 listGrade.append((list1[i], 'A+'))
                 ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                listA1.append(list1[i])
+                A1 += 1
             else:
                 listGrade.append((list1[i],'A0'))
                 ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                A2 += 1
         elif i <= lenB:
             if i <= (lenB-lenA) //2 + lenA:
                 listGrade.append((list1[i], 'B+'))
                 ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                listB1.append(list1[i])
+                B1 += 1
             else:
                 listGrade.append((list1[i],'B0'))
                 ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                B2 += 1
         else:
             if i <= (lenC-lenB) //2 + lenB:
                 listGrade.append((list1[i], 'C+'))
                 ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                listC1.append(list1[i])
+                C1 += 1
             else:
                 listGrade.append((list1[i],'C0'))
                 ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                C2 += 1
+print(listC1)
+if A1 > A2:
+    for i in range((A1+A2)//2):
+        ws.cell(row=list2.index(listA1[-1])+1, column=8, value='A0')
+        listA1.pop()
+elif B1 > B2:
+    for i in range((B1+B2)//2):
+        ws.cell(row=list2.index(listB1[-1])+1, column=8, value='B0')
+        listB1.pop()
+elif C1 > C2:
+    for i in range((C1+C2)//2):
+        ws.cell(row=list2.index(listC1[-1])+1, column=8, value='C0')
+        listC1.pop()
+
 wb.save('student.xlsx')
