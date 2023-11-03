021a22571311cd9723dac895674a1a2dcfe4b4b9
ssuisig <jsy6430000@naver.com>
2023-11-03 21:10:18 +0900

    HW1_up5

--- HW1/student20210811.py
+++ HW1/student20210811.py
@@ -16,6 +16,10 @@ B2=0
 C2=0
 listGrade.append(('0',0))
 
+def scoreCheck(i):
+    startIndex = list2.index(listGrade[i][0])+1
+    ws.cell(row=list2[startIndex:].index(listGrade[i][0])+1+startIndex, column=8, value=listGrade[i][1])
+
 ws = wb.active
 for row in range(2, 76):
     i = 0
@@ -36,39 +40,66 @@ for i in range(1, len(list1)):
     lenB = int((len(list1)-1)*0.7)
     lenC = len(list1)-1
     if list1[i] < 40:
-        listGrade.append((list1[i], 'F'))
-        if listGrade[i][0] in list2:
+        if(list1[i], 'F') in listGrade:
+            listGrade.append((list1[i], 'F'))
+            scoreCheck(i)
+        else:
+            listGrade.append((list1[i], 'F'))
             ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
     else:
         if i <= lenA:
             if i <= lenA //2:
-                listGrade.append((list1[i], 'A+'))
-                ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
-                listA1.append(list1[i])
+                if (list1[i], 'A+') in listGrade:
+                    listGrade.append((list1[i], 'A+'))
+                    scoreCheck(i)
+                else:
+                    listGrade.append((list1[i], 'A+'))
+                    ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                    listA1.append(list1[i])
                 A1 += 1
             else:
-                listGrade.append((list1[i],'A0'))
-                ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                if (list1[i], 'A0') in listGrade:
+                    listGrade.append((list1[i], 'A0'))
+                    scoreCheck(i)
+                else:
+                    listGrade.append((list1[i],'A0'))
+                    ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
                 A2 += 1
         elif i <= lenB:
             if i <= (lenB-lenA) //2 + lenA:
-                listGrade.append((list1[i], 'B+'))
-                ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
-                listB1.append(list1[i])
+                if (list1[i], 'B+') in listGrade:
+                    listGrade.append((list1[i], 'B+'))
+                    scoreCheck(i)
+                else:
+                    listGrade.append((list1[i], 'B+'))
+                    ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                    listB1.append(list1[i])
                 B1 += 1
             else:
-                listGrade.append((list1[i],'B0'))
-                ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                if (list1[i], 'B0') in listGrade:
+                    listGrade.append((list1[i], 'B0'))
+                    scoreCheck(i)
+                else:
+                    listGrade.append((list1[i],'B0'))
+                    ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
                 B2 += 1
         else:
             if i <= (lenC-lenB) //2 + lenB:
-                listGrade.append((list1[i], 'C+'))
-                ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
-                listC1.append(list1[i])
+                if (list1[i], 'C+') in listGrade:
+                    listGrade.append((list1[i], 'C+'))
+                    scoreCheck(i)
+                else:
+                    listGrade.append((list1[i], 'C+'))
+                    ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                    listC1.append(list1[i])
                 C1 += 1
             else:
-                listGrade.append((list1[i],'C0'))
-                ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
+                if (list1[i], 'C0') in listGrade:
+                    listGrade.append((list1[i], 'C0'))
+                    scoreCheck(i)
+                else:
+                    listGrade.append((list1[i],'C0'))
+                    ws.cell(row=list2.index(listGrade[i][0])+1, column=8, value=listGrade[i][1])
                 C2 += 1
 if A1 > A2:
     for i in range((A1+A2)//2):
