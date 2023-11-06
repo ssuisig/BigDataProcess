194911d46b9785da769c9f181e7dcee9633c58f0
ssuisig <jsy6430000@naver.com>
2023-11-07 08:11:46 +0900

    HW2_movie

--- HW2/IMDBStudent20210811.py
+++ HW2/IMDBStudent20210811.py
@@ -1,19 +1,27 @@
 #!usr/bin/python3
 genreDict = dict()
 genreList = []
+resultList = []
 
-f = open("E:/bigdata/BigDataProcess/HW2/movies_exp.txt", "rt")
-f2 = open("E:/bigdata/BigDataProcess/HW2/movieoutput.txt", "wt")
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
 
-for line in f:
-    genreList = (line[line.rfind(':')+1:]).rstrip('\n').split('|')
-    for i in genreList:
-        if i in genreDict:
-            genreDict[i] += 1
-        else:
-            genreDict[i] = 1
-for i in genreDict:
-    f2.write('%s %s\n'%(i, genreDict[i]))
+# f = open("E:/bigdata/BigDataProcess/HW2/movies_exp.txt", "rt")
+# f2 = open("E:/bigdata/BigDataProcess/HW2/movieoutput.txt", "wt")
 
-f.close
-f2.close
+with open(inputFile, "rt") as f:
+    for line in f:
+        genreList = (line[line.rfind(':')+1:]).rstrip('\n').split('|')
+        for i in genreList:
+            if i in genreDict:
+                genreDict[i] += 1
+            else:
+                genreDict[i] = 1
+    for i in genreDict:
+        resultStr = str(i)+' '+str(genreDict[i])
+        resultList.append(resultStr)
+print(resultStr)
+
+with open(outputFile, "wt") as fp:
+    for i in resultStr:
+        print(i)
