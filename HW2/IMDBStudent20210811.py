f9282720312cebff80446cc0007f087324aaf654
ssuisig <jsy6430000@naver.com>
2023-11-06 21:43:04 +0900

    HW2_up1

--- HW2/IMDBStudent20210811.py
+++ HW2/IMDBStudent20210811.py
@@ -1,11 +1,11 @@
+#!usr/bin/python3
 genreDict = dict()
 genreList = []
 
-f = open("E:/bigdata/BigDataProcess/HW2/movies_exp.txt", "rt")
-f2 = open("E:/bigdata/BigDataProcess/HW2/movieoutput.txt", "wt")
+f = open("movies_exp.txt", "rt")
+f2 = open("movieoutput.txt", "wt")
 for line in f:
     genreList = line[line.rfind(':')+1:-1].split('|')
-    print(genreList)
     for i in genreList:
         if i in genreDict:
             genreDict[i] += 1
@@ -14,6 +14,5 @@ for line in f:
 for i in genreDict:
     f2.write('%s %s\n'%(i, genreDict[i]))
 
-print(genreDict)
 f.close
 f2.close
